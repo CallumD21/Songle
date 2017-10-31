adc22218bb3f87d0a2c90ea4e75856998395d4c0
callum <cazza91@hotmail.co.uk>
2017-10-31 11:24:16 +0000

    Deleted the map activity and put the new map along with its code in the navigation drawer activity

--- app/src/main/java/com/example/callum/songle/MainActivity.kt
+++ app/src/main/java/com/example/callum/songle/MainActivity.kt
@@ -1,22 +1,55 @@
 package com.example.callum.songle
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
+        OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    private lateinit var mMap: GoogleMap
+    private lateinit var mGoogleApiClient: GoogleApiClient
+    val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+    var mLocationPermissionGranted = false
+    private var mLastLocation: Location? = null
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        //Create an instance of GoogleApiClient
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -80,5 +113,86 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mGoogleApiClient.connect()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if(mGoogleApiClient.isConnected){
+            mGoogleApiClient.disconnect()
+        }
+    }
+
+    fun createLocationRequest(){
+        //Set the paramaters for the location request
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 5000
+        mLocationRequest.fastestInterval = 1000
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        //See if we can access the users location
+        val permissionCheck = ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this)
+        }
+    }
+
+    override fun onConnected(connectionHint : Bundle?) {
+        try { createLocationRequest(); }
+        catch (ise : IllegalStateException){
+            Log.d("MYAPP","IllegalStateException thrown [onConnected]")
+        }
+        //See if we can access the users location
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient)
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    override fun onLocationChanged(current : Location?) {
+        if(current == null){
+            //DO SOMETHING ELSE
+            Log.d("MYAPP","[onLocationChanged] Location is unknown")
+        } else {
+            //MAYBE CHANGE THIS
+            Log.d("MYAPP","""[onLocationChanged] Lat/Long now (${current.getLatitude()},${current.getLongitude()})""")
+        }
+        //DO SOMETHING
+    }
+
+    override fun onConnectionSuspended(flag : Int) {
+        //DO SOMETHING
+        Log.d("MYAPP","[onConnectionSuspended]")
+    }
+
+    override fun onConnectionFailed(result: ConnectionResult) {
+        //DO SOMETHING
+        Log.d("MYAPP","[onConnectionFailed]")
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        try {
+            //Show current position
+            mMap.isMyLocationEnabled = true
+        } catch (se : SecurityException) {
+            Log.d("MYAPP","Security exception thrown [onMapReady]")
+        }
+        //Add ”My location” button to the user interface
+        mMap.uiSettings.isMyLocationButtonEnabled = true
     }
 }
